bed0fb40381e2d747bf8d833be5ea43c2c0c780d
Svetlana Shmyhol <art.firefly@gmail.com>
2024-08-14 11:50:25 +0300

    Mobile adaptation

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,14 +1,15 @@
 function mobileNav() {
-	// Mobile nav button
-	const navBtn = document.querySelector('.mobile-nav-btn');
-	const nav = document.querySelector('.mobile-nav');
-	const menuIcon = document.querySelector('.nav-icon');
+  // Mobile nav button
+  const navBtn = document.querySelector('.mobile-nav-btn');
+  const nav = document.querySelector('.mobile-nav');
+  const menuIcon = document.querySelector('.nav-icon');
+  const menuLinks = document.querySelector('.nav-list_link');
 
-	navBtn.onclick = function () {
-		nav.classList.toggle('mobile-nav--open');
-		menuIcon.classList.toggle('nav-icon--active');
-		document.body.classList.toggle('no-scroll');
-	};
+  navBtn.onclick = function () {
+    nav.classList.toggle('mobile-nav--open');
+    menuIcon.classList.toggle('nav-icon--active');
+    document.body.classList.toggle('no-scroll');
+  };
 }
 
 export default mobileNav;
